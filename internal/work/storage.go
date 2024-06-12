b13fb411636c5f16a41bddfed650311b3812f859
onee-only <kimww0306@gmail.com>
2024-06-12 20:22:24 +0900

    Add work package

---
+++ internal/work/storage.go
@@ -0,0 +1,12 @@
+package work
+
+import (
+	"context"
+
+	"github.com/google/uuid"
+)
+
+type Storage interface {
+	FetchTemplates(ctx context.Context, taskID uuid.UUID, sectionID uuid.UUID) (templates map[uuid.UUID]Template, err error)
+	Stream(ctx context.Context, taskID uuid.UUID, sectionID uuid.UUID) (stream <-chan *Work, err error)
+}
