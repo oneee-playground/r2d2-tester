63d913498759446bb41d07362e7f3ba6b93e6dd1
onee-only <kimww0306@gmail.com>
2024-07-12 03:07:56 +0000

    Add cd workflow

---
+++ Dockerfile
@@ -0,0 +1,17 @@
+FROM golang:1.22 AS build
+
+WORKDIR /build
+
+COPY ./cmd ./cmd
+COPY ./internal ./internal
+COPY go.mod go.mod
+COPY go.sum go.sum 
+
+RUN CGO_ENABLED=0 GOOS=linux go build -o app ./cmd
+
+
+FROM alpine:latest
+
+COPY --from=build /build/app ./app
+
+ENTRYPOINT [ "./app" ]
--- internal/config/load.go
+++ internal/config/load.go
@@ -8,7 +8,7 @@ func LoadFromEnv() {
 	WorkStoragePath = os.Getenv("WORK_STORAGE_PATH")
 
 	InfluxURL = os.Getenv("INFLUX_URL")
-	InfluxToken = os.Getenv("INFLUX_PATH")
+	InfluxToken = os.Getenv("INFLUX_TOKEN")
 
 	JobQueueURL = os.Getenv("JOB_QUEUE_URL")
 	EventQueueURL = os.Getenv("EVENT_QUEUE_URL")
